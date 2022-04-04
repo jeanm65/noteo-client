78c76384059a15c4d47faae7a18b3696d74afcb2
tiavina-mika <tiavinamika@gmail.com>
2022-04-04 10:42:57 +0300

    refactor routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,9 @@
-import { useState } from 'react';
-import './styles/loginStyle.css';
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import Home from './containers/Home';
-import Login from './containers/Login';
-import Notes from './containers/Notes';
-import PublicRoute from './PublicRoute';
+import Routes from './Routes';
+import './styles.css';
 
 const App = () => {
-  const [values, setValues] = useState({
-    mail: '',
-    password: '',
-  });
-  const [checkValue, setCheckValue] = useState(true);
-  const inputs = [
-    {
-      id: 1,
-      name: 'mail',
-      errorMessage: 'The email is not valid',
-      type: 'mail',
-      label: 'Email',
-      pattern: '/^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/',
-      required: true,
-    },
-    {
-      id: 2,
-      name: 'password',
-      errorMessage: 'Password should be minimum eight characters, at least one uppercase letter, one lowercase letter and one number:',
-      type: 'password',
-      label: 'Password',
-      pattern: '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})',
-      required: true,
-    },
-  ];
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
-  const onChange = () => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
-  const checkOnchange = (e) => {
-    setCheckValue(e.currentTarget.checked);
-  };
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<PublicRoute component={Home} />} />
-        <Route path="/notes" element={<PublicRoute component={Notes} />} />
-        <Route
-          path="/login"
-          element={
-            <div className="form">
-              <form onSubmit={handleSubmit}>
-                <h2>LOGIN</h2>
-                {inputs.map((input) => (
-                  <Login
-                    key={input.id}
-                    {...input}
-                    value={values[input]}
-                    onChange={onChange}
-                  />
-                  ))}
-                <div className="check">
-                  <input type="checkbox" name="checkbox" id="3" value={checkValue} onChange={checkOnchange} />
-                  <label htmlFor="checkbox">Remember me?</label>
-                </div>
-                <div className="submit">
-                  <input type="submit" value="LOGIN" />
-                </div>
-                <h4>Forgot password ?</h4>
-                <h5>OR</h5>
-              </form>
-            </div>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    <Routes />
   );
 };
 
--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,23 +1,74 @@
-const Login = (props) => {
-  const [focused, setFocused] = useState(false);
-  const handleFocused = () => {
-    setFocused(true);
+import { useState } from 'react';
+
+const Login = () => {
+  const [values, setValues] = useState({
+    email: '',
+    password: '',
+    rememberMe: false,
+  });
+
+  const [errors, setErrors] = useState({
+    mail: '',
+    password: '',
+  });
+
+  // const [checkValue, setCheckValue] = useState(true);
+  const handleSubmit = (e) => {
+    console.log('values', values);
+    e.preventDefault();
   };
-  const {
- label, errorMessage, onChange, id, ...inputProps
-} = props;
+
+  const onChange = () => {
+    if (!e.target.value) {
+      setErrors({ ...errors, [e.target.name]: e.target.value });
+    }
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const checkOnchange = (e) => {
+    setValues({ ...values, rememberMe: e.currentTarget.checked });
+  };
+
   return (
-    <div className="inputs">
-      <div className="main">
-        <label>{label}</label>
-        <input
-          key={id}
-          {...inputProps}
-          onChange={onChange}
-          onBlur={handleFocused}
-          focused={focused.toString()}
-        />
-        <span>{errorMessage}</span>
+    <div className="container">
+      <div className="card">
+        <div className="titleContainer">
+          <h2>LOGIN</h2>
+        </div>
+        <form onSubmit={handleSubmit} className="form">
+          <div className="formControl">
+            <label>Email</label>
+            <input
+              name="email"
+              type="email"
+              value={values.email}
+              onChange={onChange}
+              className="field"
+            />
+            {errors.email && <span className="errorMessage">{errors.email}</span>}
+          </div>
+
+          <div className="formControl">
+            <label>Mot de passe</label>
+            <input
+              name="password"
+              type="password"
+              value={values.password}
+              onChange={onChange}
+              className="field"
+            />
+            {errors.password && <span className="errorMessage">{errors.password}</span>}
+          </div>
+
+          <div className="formControl checkboxContainer">
+            <input type="checkbox" name="checkbox" value={values.rememberMe} onChange={checkOnchange} className="checkboxInput" />
+            <label htmlFor="checkbox">Remember me?</label>
+          </div>
+
+          <div className="formControl buttonContainer">
+            <input type="submit" value="LOGIN" className="button" />
+          </div>
+        </form>
       </div>
     </div>
   );
