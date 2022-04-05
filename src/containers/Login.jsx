3ac0d057f8feb74e5db5a455e12ff66d6bcdc8b6
jeanm65 <70010768+jeanm65@users.noreply.github.com>
2022-04-05 13:01:53 +0300

    some modifications

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => (
         rememberMe: '',
       }}
       onSubmit={(values) => {
-        console.log(JSON.stringify(values, null, 2));
+        console.log(JSON.stringify(values));
       }}
       validationSchema={Yup.object().shape({
         email: Yup.string().email().required('Required'),
