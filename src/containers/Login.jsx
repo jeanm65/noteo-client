81695db199a874aecabf05e2e6110e5ff5de2fff
jeanm65 <70010768+jeanm65@users.noreply.github.com>
2022-04-06 17:10:57 +0300

    creation of loginForm as a reusable form

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,64 +1,17 @@
-import { React } from 'react';
-import '../styles.css';
+import React from 'react';
 
-import {
- Formik, Form, Field, ErrorMessage,
-} from 'formik';
-
-import { loginValidationSchema } from '../utils/validation';
-
-const initialValues = {
-  email: '',
-  password: '',
-  rememberMe: true,
-};
+import LoginForm from '../LoginForm';
 
 const Login = () => {
-  const onSubmit = (values) => {
-    console.log('values', values);
-  };
-
   return (
-    <Formik
-      initialValues={initialValues}
-      onSubmit={onSubmit}
-      validationSchema={loginValidationSchema}
-    >
-      {({ isSubmitting }) => (
-        <div className="container">
-          <div className="card">
-            <div className="titleContainer">
-              <h2>LOGIN</h2>
-            </div>
-            <Form className="form">
-              <div className="formControl">
-                <label>Email</label>
-                <Field type="email" name="email" className="field" />
-                <ErrorMessage name="email" component="span" className="errorMessage" />
-              </div>
-
-              <div className="formControl">
-                <label>Mot de passe</label>
-                <Field type="password" name="password" className="field" />
-                <ErrorMessage name="password" component="span" className="errorMessage" />
-              </div>
-
-              <div className="formControl checkboxContainer">
-                <input type="checkbox" name="rememberMe" defaultChecked className="checkboxInput" />
-                <label htmlFor="checkbox">Remember me?</label>
-              </div>
-
-              <div className="formControl buttonContainer">
-                <button type="submit" disabled={isSubmitting} className="button">
-                  Submit
-                </button>
-              </div>
-            </Form>
-          </div>
+    <div className="container">
+      <div className="card">
+        <div className="titleContainer">
+          <h2>LOGIN</h2>
         </div>
-
-       )}
-    </Formik>
+        <LoginForm />
+      </div>
+    </div>
   );
 };
 
